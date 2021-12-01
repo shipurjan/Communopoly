a9f0f3f902b318f5ff6dafe705d0f257e57d5ee9
Nikodem Stasiewicz <nikodemstasiewicz@gmail.com>
2021-12-01 13:46:35 +0100

    Dodanie klasy Dice,rolowanko potem dodam zeby wyswietlało która kostka ile wylosowała

--- src/Library/Dice.cs
+++ src/Library/Dice.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    class Dice : PropertyChangedBase
+    class Dice
     {
+        private int die1;
+        private int die2;
+
+        public int Die1
+        {
+            get { return die1; }
+            set { die1 = value; }
+        }
+
+
+        public int Die2
+        {
+            get { return die2; }
+            set { die2 = value; }
+        }
+
+        public void Roll()
+        {
+            //get a random number object we can the use to determine the die face
+            Random rand1 = new Random();
+            Die1 = rand1.Next(1, 6);
+            Die2 = rand1.Next(1, 6);
+        }
     }
 }
+
