12b8e0e9f744253824544826c795166fea2f5db9
Nikodem Stasiewicz <nikodemstasiewicz@gmail.com>
2021-12-09 09:30:24 +0100

    Program teraz pokazuje ile wylosowała dana kostka

--- src/Library/Dice.cs
+++ src/Library/Dice.cs
@@ -6,6 +6,28 @@ using System.Threading.Tasks;
 
 namespace Library
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //welcome the user and start the program
+            Console.WriteLine("Welcome to the dice program");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Rolling dice");
+
+            //create a new dice object and roll
+            Dice newRun = new Dice();
+            newRun.Roll();
+
+            //display the result of the rolls
+            Console.WriteLine("Die 1 rolled a {0}", newRun.Die1);
+            Console.WriteLine("Die 2 rolled a {0}", newRun.Die2);
+
+
+
+
+        }
+    }
     class Dice
     {
         private int die1;
