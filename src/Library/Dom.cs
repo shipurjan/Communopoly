9112932d0c86dd65667efa1f7802fb68f80fda0a
Nikodem Stasiewicz <nikodemstasiewicz@gmail.com>
2022-01-13 16:51:14 +0100

    Stworzenie klasy 'Dom', jeśli więcej niż 4 domy to czy chcesz wymienić na Hotel

---
+++ src/Library/Dom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Dom
+    {
+        public void mortgageProperty(Player player) 
+        { 
+            PlayerInput input = new PlayerInput();
+            Monopoly game = new Monopoly();
+
+            if (player.getPropertiesOwnedFromBoard().Count == 0)
+            {
+
+                Console.WriteLine("Nie masz żadnych domków.", input.playerPrompt(player));
+
+                return;
+            }
+            else if (player.getPropertiesOwnedFromBoard().Count > 4)
+            {
+                Console.WriteLine("Czy chcesz zamienić 4 domki na hotel?");
+            }
+
+        }
+    }
+
+
+}
+
