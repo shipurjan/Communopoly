a96d602cb097ce3267b727ed7c935642e202655b
Cyprian Zdebski <57770826+shipurjan@users.noreply.github.com>
2022-02-05 14:38:32 +0100

    Przygotowania środowiska do testów

---
+++ src/LibraryTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library;
+
+namespace LibraryTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
