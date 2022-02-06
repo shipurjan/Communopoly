5fb28fff1d72b37824dd2bc9a5906b475c6e1107
Nikodem Stasiewicz <nikodemstasiewicz@gmail.com>
2022-02-06 12:17:53 +0100

    Wzorzec projektowy 2

--- src/Library/Dom.cs
+++ src/Library/Dom.cs
@@ -30,4 +30,50 @@ namespace Library
 
 
 }
+namespace RefactoringGuru.DesignPatterns.Adapter.Conceptual
+{
+    
+    public interface ITarget
+    {
+        string GetRequest();
+    }
+
+   
+    class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Specific request.";
+        }
+    }
+
+    
+    class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return $"This is '{this._adaptee.GetSpecificRequest()}'";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+
+            Console.WriteLine("Adaptee interface is incompatible with the client.");
+            Console.WriteLine("But with adapter client can call it's method.");
+
+            Console.WriteLine(target.GetRequest());
+        }
+    }
 
